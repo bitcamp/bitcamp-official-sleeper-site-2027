08c1d13cd34650aaf6887c582e04707f7009c2e1
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2025-09-07 18:13:44 -0400

    spons page updates

--- components/ProspectusMisc.vue
+++ components/ProspectusMisc.vue
@@ -1,63 +1,83 @@
 <template>
-    <div class="Rtable-cell Rtable-category-cell" :class="'Rtable-misc-category-cell-' + type">
-        <a class="tooltips direction" :title="perk" :description="description">
-            <img src="../assets/img/icons/question-mark-black.svg" alt="Question Mark" />
-        </a>
-        <span v-if="bolded" style="color:var(--color-dark-text)"><b>{{ perk }}</b></span>
-        <span v-else style="color: var(--color-dark-text);">{{ perk }}</span>
-        <div v-if="note" style="margin-top: 4px; font-size: 0.85em; color: var(--color-dark-text);">
-            {{ note }}
-        </div>
+  <div
+    class="Rtable-cell Rtable-category-cell"
+    :class="'Rtable-misc-category-cell-' + type"
+  >
+    <a class="tooltips direction" :title="perk" :description="description">
+      <img
+        src="../assets/img/icons/question-mark-black.svg"
+        alt="Question Mark"
+      />
+    </a>
+    <div>
+      <span v-if="bolded" style="color: var(--color-dark-text)"
+        ><b>{{ perk }}</b></span
+      >
+      <span v-else style="font-size: 15.5px; color: var(--color-dark-text)">{{
+        perk
+      }}</span>
+      <div
+        v-if="note"
+        style="
+          margin-top: 4px;
+          font-size: 0.85em;
+          color: var(--color-dark-text);
+        "
+      >
+        {{ note }}
+      </div>
     </div>
+  </div>
 
-    <div class="Rtable-cell Rtable-cell-misc" :class="'Rtable-misc-cell-' + type"
-        style="color: var(--color-dark-text);">
-        {{ price }}
-    </div>
-    <div class="Rtable-cell Rtable-cell-blank"></div>
-    <div class="Rtable-cell Rtable-cell-blank"></div>
-    <div class="Rtable-cell Rtable-cell-blank"></div>
-    <div class="Rtable-cell Rtable-cell-blank"></div>
-    <div class="Rtable-cell Rtable-cell-blank"></div>
-    
-    
+  <div
+    class="Rtable-cell Rtable-cell-misc"
+    :class="'Rtable-misc-cell-' + type"
+    style="color: var(--color-dark-text)"
+  >
+    {{ price }}
+  </div>
+  <div class="Rtable-cell Rtable-cell-blank"></div>
+  <div class="Rtable-cell Rtable-cell-blank"></div>
+  <div class="Rtable-cell Rtable-cell-blank"></div>
+  <div class="Rtable-cell Rtable-cell-blank"></div>
+  <div class="Rtable-cell Rtable-cell-blank"></div>
 </template>
 
 <script>
 export default {
-    name: 'ProspectusFooter',
-    props: {
-        perk: {
-            type: String,
-            required: true,
-        },
-        bolded: {
-            type: Boolean,
-            required: true
-        },
-        price: {
-            type: String,
-            required: true,
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        note: { 
-            type: String,
-            required: false,
-            default: ''
-        },
+  name: "ProspectusFooter",
+  props: {
+    perk: {
+      type: String,
+      required: true,
     },
+    bolded: {
+      type: Boolean,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
 };
 </script>
 
 <style scoped>
 .boldClass {
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
